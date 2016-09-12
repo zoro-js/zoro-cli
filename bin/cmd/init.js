e5cbe590c084eb34b665307b80ea89da2845426e
Yinyga Zhang <zyy7259@gmail.com>
2016-09-12 10:10:59 +0800

    add files

--- bin/cmd/init.js
+++ bin/cmd/init.js
@@ -7,12 +7,23 @@ const dirs = [
   'src/html',
   'dist'
 ]
+const files = [
+  'src/js/entry/index.js',
+  'src/js/entry/foo.js',
+  'src/postcss/entry/index.css',
+  'src/postcss/entry/common.css',
+  'src/html/index.html'
+]
 
 exports.command = 'init'
 exports.desc = 'init dirs'
 exports.builder = {}
 exports.handler = function (argv) {
-  Promise.map(dirs, dir => {
+  const p1 = Promise.map(dirs, dir => {
     return fs.ensureDirAsync(dir)
   })
+  const p2 = Promise.map(files, file => {
+    return fs.ensureFileAsync(file)
+  })
+  return Promise.all([p1, p2])
 }
